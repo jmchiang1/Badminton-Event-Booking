1d513f9b1ccd7f2c45016adfe06c9f284d5647c4
Jonathan Chiang <jonathanchiang7@gmail.com>
2021-08-19 15:14:45 -0400

    test commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -13,21 +14,19 @@ app.use(
   '/graphql',
   graphqlHTTP({
     schema: buildSchema(`
-    type Event {
-        _id: ID!
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-    }
-
-    input EventInput {
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-    }
-
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
         type RootQuery {
             events: [Event!]!
         }
@@ -43,20 +42,20 @@ app.use(
       events: () => {
         return events;
       },
-      createEvent: (args) => {
+      createEvent: args => {
         const event = {
-            _id: Math.random().toString(),
-            title: args.eventInput.title,
-            description: args.eventInput.description,
-            price: +args.eventInput.price,
-            date: args.eventInput.date
-        }
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
         events.push(event);
         return event;
       }
     },
     graphiql: true
-  }) 
+  })
 );
 
 app.listen(3000);
